rapier1/HPN-SSH

commit 4e935fb15894fefef03529d69a0c246e39bc87c8

Improve performance of ChaCha20 "forks"
Collapse interprocess communication into as few messages as possible in
performance-critical sections.

Avoid re-allocating new large arrays on the stack during
performance-critical sections.

ssh-ccp-helper.c
@@ -296,7 +296,7 @@ main(int argc, char ** argv) {
 				}
 				break;
 			case 'p' :
-				/* read poly_key and keystream */
+				/* read poly_key and keystream, then advance */
 				/* FALL THROUGH */
 			case 'r' :
 				/* read keystream */
@@ -323,6 +323,12 @@ main(int argc, char ** argv) {
 				    param + AADLEN) != param + AADLEN)
 					quitting = 1;
 				err("wrote xorStream\n");
+				if(cmd == 'p') {
+					/* queue generation of next keystream */
+					ungetc('g',stdin);
+					/* queue incrementing seqnr */
+					ungetc('n',stdin);
+				}
 				break;
 			default :
 				/* unrecognized command */

cipher-chachapoly-libcrypto-forks.c
@@ -47,6 +47,8 @@
 #define LIKELY(x)   __builtin_expect((x),1)
 #define UNLIKELY(x) __builtin_expect((x),0)
 
+#define PUT_UINT(ADDR, VAL) ( *((u_int *) (ADDR)) = (VAL) )
+
 /* #define WORKERPATH "ssh-worker-chacha20" */
 
 struct chachapoly_ctx {
@@ -60,6 +62,8 @@ struct chachapolyf_ctx {
 	int (* wpipes)[2];
 	u_int nextseqnr;
 	pid_t * pids;
+	u_char sndbuf[4096];
+	u_char rcvbuf[POLY1305_KEYLEN + AADLEN + KEYSTREAMLEN];
 };
 
 void
@@ -326,34 +330,29 @@ chachapolyf_crypt(struct chachapoly_ctx *cp_ctx, u_int seqnr, u_char *dest,
 	}
 	struct chachapolyf_ctx * ctx = cp_ctx->cpf_ctx;
 
-/*	dumphex("src (AADLEN)", src, AADLEN); */
+	u_char * poly_key = ctx->rcvbuf;
+	u_char * xorStream = &(ctx->rcvbuf[POLY1305_KEYLEN]);
 
-	u_char xorStream[KEYSTREAMLEN + AADLEN];
 	u_char expected_tag[POLY1305_TAGLEN];
-	u_char poly_key[POLY1305_KEYLEN];
+
 	int r = SSH_ERR_INTERNAL_ERROR;
 	if (UNLIKELY(ctx->nextseqnr != seqnr)) {
+		ctx->sndbuf[0] = 's';
+		ctx->sndbuf[5] = 'g';
 		for (u_int i = 0; i < ctx->numworkers; i++) {
-			if (UNLIKELY(pcw(ctx, (seqnr + i) % ctx->numworkers, 's')))
-				goto out;
-			if (UNLIKELY(piw(ctx, (seqnr + i) % ctx->numworkers, seqnr + i)))
-				goto out;
-			if (UNLIKELY(pcw(ctx, (seqnr + i) % ctx->numworkers, 'g')))
+			PUT_UINT(&(ctx->sndbuf[1]), seqnr + i);
+			if (UNLIKELY(pw(ctx, (seqnr + i) % ctx->numworkers,
+			    ctx->sndbuf, 6)))
 				goto out;
 		}
 		ctx->nextseqnr = seqnr;
 	}
-	if (UNLIKELY(pcw(ctx, seqnr % ctx->numworkers, 'p')))
+	ctx->sndbuf[0]='p';
+	PUT_UINT(&(ctx->sndbuf[1]), len);
+	if (UNLIKELY(pw(ctx, seqnr % ctx->numworkers, ctx->sndbuf, 5)))
 		goto out;
-	if (UNLIKELY(piw(ctx, seqnr % ctx->numworkers, len)))
-		goto out;
-	if (UNLIKELY(pr(ctx, seqnr % ctx->numworkers, poly_key, POLY1305_KEYLEN)))
-		goto out;
-/*	dumphex("poly_key",poly_key,POLY1305_KEYLEN); */
-	if (UNLIKELY(pr(ctx, seqnr % ctx->numworkers, xorStream, len + AADLEN)))
-		goto out;
-/*	dumphex("xorStream (AADLEN)", xorStream, AADLEN); */
-	if (UNLIKELY(pw(ctx, seqnr % ctx->numworkers, "ng", 2)))
+	if (UNLIKELY(pr(ctx, seqnr % ctx->numworkers, ctx->rcvbuf,
+	    POLY1305_KEYLEN + AADLEN + len)))
 		goto out;
 
 	/* If decrypting, check tag before anything else */
@@ -368,7 +367,8 @@ chachapolyf_crypt(struct chachapoly_ctx *cp_ctx, u_int seqnr, u_char *dest,
 	}
 
 	u_int last4 = (len + AADLEN)/4 - 1;
-	((uint32_t *) dest)[last4] = ((uint32_t *) xorStream)[last4] ^ ((uint32_t *) src)[last4];
+	((uint32_t *) dest)[last4] =
+	    ((uint32_t *) xorStream)[last4] ^ ((uint32_t *) src)[last4];
 	typedef uint64_t bsize;
 	bsize * destB      = (bsize *) &(dest[0]);
 	bsize * srcB       = (bsize *) &(src[0]);
@@ -389,10 +389,11 @@ chachapolyf_crypt(struct chachapoly_ctx *cp_ctx, u_int seqnr, u_char *dest,
 	ctx->nextseqnr = seqnr + 1;
 	r = 0;
  out:
-	explicit_bzero(xorStream, sizeof(xorStream));
+	if (ctx == NULL)
+		fprintf(stderr,"CTX NULL!\n");
+	explicit_bzero(ctx->rcvbuf,
+	    (POLY1305_KEYLEN + len + aadlen) * sizeof(u_char));
 	explicit_bzero(expected_tag, sizeof(expected_tag));
-	explicit_bzero(poly_key, sizeof(poly_key));
-/*	dumphex("dest (AADLEN)", dest, AADLEN); */
 	return r;
 }
 

commit c2b7d8d9a7e5ac4f5a7f7a17f65a8b58dc4be448

Fix broken chachapoly helper
Fix an error introduced in 4e935fb15894fefef03529d69a0c246e39bc87c8. The
ungetc() function shouldn't be used in that context, so the keystream
generation routine was refactored into its own function, which now gets
called at the end of the 'p' case.

ssh-ccp-helper.c
@@ -131,11 +131,55 @@ int readBytes(u_char * result, size_t size, u_char textmode) {
 }
 
 int
-main(int argc, char ** argv) {
+gen(u_char * poly_key, u_char * xorStream, u_int seqnr,
+    EVP_CIPHER_CTX * main_evp, EVP_CIPHER_CTX * header_evp) {
 	u_char zeros[KEYSTREAMLEN + AADLEN];
+	u_char seqbuf[16];
+	int ret;
+
+	ret = 0;
+	memset(seqbuf, 0, sizeof(seqbuf));
+	memset(zeros, 0, sizeof(zeros));
+
+	POKE_U64(seqbuf + 8, seqnr);
+	memset(poly_key, 0, POLY1305_KEYLEN);
+
+	if (!EVP_CipherInit(main_evp, NULL, NULL, seqbuf, 1)) {
+		ret = 1;
+		goto out;
+	}
+	if (EVP_Cipher(main_evp, poly_key, poly_key, POLY1305_KEYLEN) < 0) {
+		ret = 1;
+		goto out;
+	}
+	if (!EVP_CipherInit(header_evp, NULL, NULL, seqbuf, 1)) {
+		ret = 1;
+		goto out;
+	}
+	if (EVP_Cipher(header_evp, xorStream, zeros, CHACHA_BLOCKLEN) < 0 ) {
+		ret = 1;
+		goto out;
+	}
+	seqbuf[0] = 1;
+	if (!EVP_CipherInit(main_evp, NULL, NULL, seqbuf, 1)) {
+		ret = 1;
+		goto out;
+	}
+	if (EVP_Cipher(main_evp, xorStream + AADLEN, zeros, KEYSTREAMLEN) < 0) {
+		ret = 1;
+		goto out;
+	}
+ out:
+	if (ret == 1)
+		err("failed in g.\n");
+	explicit_bzero(seqbuf, sizeof(seqbuf));
+	return ret;
+}
+
+int
+main(int argc, char ** argv) {
 	EVP_CIPHER_CTX * main_evp;
 	EVP_CIPHER_CTX * header_evp;
-	u_char seqbuf[16];
 	u_int seqnr;
 
 	u_char poly_key[POLY1305_KEYLEN];
@@ -152,7 +196,6 @@ main(int argc, char ** argv) {
 
 	u_char quitting;
 
-	memset(zeros,0,sizeof(zeros));
 	main_evp = NULL;
 	header_evp = NULL;
 
@@ -254,46 +297,8 @@ main(int argc, char ** argv) {
 			case 'g' :
 				/* genererate keystream */
 				err("received g\n");
-				memset(seqbuf,0,sizeof(seqbuf));
-				POKE_U64(seqbuf+8,seqnr);
-				memset(poly_key,0,sizeof(poly_key));
-				if(!EVP_CipherInit(main_evp, NULL, NULL, seqbuf,
-				    1)) {
-					err("failed in g.\n");
-					quitting=1;
-					break;
-				}
-				if(EVP_Cipher(main_evp, poly_key, poly_key,
-				    sizeof(poly_key)) < 0) {
-					err("failed in g.\n");
-					quitting=1;
-					break;
-				}
-				if(!EVP_CipherInit(header_evp, NULL, NULL,
-				    seqbuf, 1)) {
-					err("failed in g.\n");
-					quitting=1;
-					break;
-				}
-				if(EVP_Cipher(header_evp, xorStream, zeros,
-				    CHACHA_BLOCKLEN) < 0 ) {
-					err("failed in g.\n");
-					quitting=1;
-					break;
-				}
-				seqbuf[0] = 1;
-				if(!EVP_CipherInit(main_evp, NULL, NULL, seqbuf,
-				    1)) {
-					err("failed in g.\n");
-					quitting=1;
-					break;
-				}
-				if(EVP_Cipher(main_evp, xorStream + AADLEN,
-				    zeros, KEYSTREAMLEN) < 0) {
-					err("failed in g.\n");
-					quitting=1;
-					break;
-				}
+				quitting = gen(poly_key, xorStream, seqnr,
+				    main_evp, header_evp);
 				break;
 			case 'p' :
 				/* read poly_key and keystream, then advance */
@@ -324,10 +329,11 @@ main(int argc, char ** argv) {
 					quitting = 1;
 				err("wrote xorStream\n");
 				if(cmd == 'p') {
-					/* queue generation of next keystream */
-					ungetc('g',stdin);
-					/* queue incrementing seqnr */
-					ungetc('n',stdin);
+					err("advancing seqnr\n");
+					seqnr += streams;
+					err("calling gen\n");
+					quitting = gen(poly_key, xorStream,
+					    seqnr, main_evp, header_evp);
 				}
 				break;
 			default :
@@ -345,7 +351,6 @@ main(int argc, char ** argv) {
 	err("cleaning up.\n");
 	EVP_CIPHER_CTX_free(main_evp);
 	EVP_CIPHER_CTX_free(header_evp);
-	explicit_bzero(seqbuf,sizeof(seqbuf));
 	explicit_bzero(poly_key,sizeof(poly_key));
 	explicit_bzero(headerkey,sizeof(headerkey));
 	explicit_bzero(mainkey,sizeof(mainkey));